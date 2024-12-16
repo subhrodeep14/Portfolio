abed37ab5e981c9d84bd26eca9a5905bed3976ad
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2024-12-17 00:07:00 +0530

    navbar updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,26 @@
 import Navbar from "./Navbar";
 
 function Hero() {
-    return (
-      <div className="min-h-screen bg-zinc-800 text-yellow-50  ">
-        <Navbar/>
-        <h1 className="text-4xl font-bold">Hi, I'm Subhrodeep</h1>
-        <p className="text-lg mt-4">Full-Stack Developer | React | Tailwind CSS</p>
-        <a href="#contact" className="mt-6 inline-block bg-white text-blue-500 px-6 py-3 rounded-lg font-semibold hover:bg-gray-200">
-          Contact Me
-        </a>
-  
+  return (
+    <div className="min-h-screen bg-zinc-900 text-yellow-50  ">
+      <Navbar />
+      <div className="flex justify-center items-center align-middle mt-40">
+        <div>
+          <h1 className="text-7xl font-bold">Hi, I'm <span className="text-blue-500">Subhrodeep</span> </h1>
+          <p className="text-lg mt-4">I develop open-source full-stack apps/tools and occasionally take on freelance projects.</p>
+          <a href="#contact" className="mt-6 inline-block bg-white text-blue-500 px-6 py-3 rounded-lg font-semibold hover:bg-gray-200">
+            Contact Me
+          </a>
+          <button class="px-10 py-3 ml-6 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2">
+            MY CV
+          </button>
+
+
+        </div>
 
       </div>
-    );
-  }
-  
-  export default Hero;
-  
+    </div>
+  );
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import {assets} from "../assets/assets"
+import { assets } from "../assets/assets"
 import Button from './Button';
 
 function Navbar() {
   return (
-    
-    <nav className="bg-zinc-800 p-4 text-white flex ">
-       
-      <div className="container mx-auto flex  items-center  ">
+
+    <div className="bg-zinc-900 p-4 text-white ">
+
+      <div className="container mx-auto flex items-center ">
         <div className=''>
-          <h1 className='text-3xl ml-10 font-bold'>SUBHRO<span className='text-blue-500'>DEEP.</span></h1>
+          <h1 className='text-4xl ml-10 font-bold'>SUBHRO<span className='text-blue-500'>DEEP.</span></h1>
         </div>
-        <div className=' w-full flex justify-center '>
-          <ul className="flex space-x-12  items-center font-medium text-xl ">
-          <li><Link href="/home" className="hover:underline">Home</Link></li>
-          <li><Link href="/about" className="hover:underline">About</Link></li>
-          <li><Link href="/projects" className="hover:underline">Projects</Link></li>
-          <li><Link href="/skills" className="hover:underline">Skills</Link></li>
-          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
+        <div className=' w-full flex justify-center items-center '>
+          <ul className="flex space-x-12  p-4 mr-44 border-2 rounded-xl mt-3  items-center font-medium text-xl ">
+            <li><Link href="/home" className="hover:underline hover:text-blue-500">Home</Link></li>
+            <li><Link href="/about" className="hover:underline hover:text-blue-500">About</Link></li>
+            <li><Link href="/projects" className="hover:underline hover:text-blue-500">Projects</Link></li>
+            <li><Link href="/skills" className="hover:underline hover:text-blue-500">Skills</Link></li>
+            <li><Link href="/contact" className="hover:underline hover:text-blue-500">Contact</Link></li>
           </ul>
         </div>
-        <div className='flex justify-center p-2 gap-3 mr-10 text-xl'>
-        <button className='py-2 px-10 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border bg-blue-500 border-blue-600 text-white hover:bg-zinc-800 hover:text-blue-500 '><span className=''>RESUME</span></button>
-          <button className='py-2 px-10 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-blue-600 text-blue-600 hover:bg-blue-500 hover:text-white '>SHARE</button>
-        </div> 
-        
-        
-        
+   
+
+
+
       </div>
-    </nav>
+     
+    </div>
   );
 }
 
