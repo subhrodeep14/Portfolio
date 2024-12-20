eb1b54a86b43550a9bdceb623d014ed7d1bf1d54
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2024-12-20 17:55:30 +0530

    skill updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,21 +6,27 @@ function Skills() {
     return (
       <section id="skills" className="h-screen bg-zinc-900 text-white">
         <div className="container mx-auto text-center">
-          <h2 className="text-5xl font-bold ">My <span className="text-blue-500 underline underline-offset-4">Skills</span> </h2>
+          <h2 className="text-5xl font-bold pt-5 ">My <span className="text-blue-500 underline underline-offset-4">Skills</span> </h2>
           <p className="font-semibold text-zinc-500 mb-10 mt-5">Crafting full stack web apps using M.E.R.N stack</p>
           <div className="mt-3 flex flex-wrap justify-center gap-10 ">
+            <div className="flex flex-wrap justify-center gap-5 ">
             <CardForSkills text="Html" image={icon.html}/>
             <CardForSkills text="Css" image={icon.css}/>
             <CardForSkills text="Javascript" image={icon.javascript}/>
             <CardForSkills text="Nodejs" image={icon.nodejs}/>
             <CardForSkills text="Express" image={icon.express}/>
             <CardForSkills text="Mongodb" image={icon.mongodb}/>
+
+            </div>
+            <div className="flex flex-wrap justify-center gap-5">
+            
             <CardForSkills text="React" image={icon.react}/>
             <CardForSkills text="Tailwind" image={icon.tailwind}/>
             <CardForSkills text="Postgresql" image={icon.postgresql}/>
             <CardForSkills text="Prisma" image={icon.prisma}/>
             <CardForSkills text="Nextjs" image={icon.nextjs}/>
             <CardForSkills text="Postman" image={icon.postman}/>
+            </div>
           </div>
         </div>
       </section>
