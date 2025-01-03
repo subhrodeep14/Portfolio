393639f6d9bd7a7540581986a9016e17b61a4899
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-01-03 23:53:07 +0530

    dark mode incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
@@ -10,8 +10,28 @@ import Navbar from './components/Navbar';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const getDefaultTheme = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+  const [theme, setTheme] = useState(getDefaultTheme());
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = () => setTheme(mediaQuery.matches ? "dark" : "light");
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className='bg-zinc-900'>
+    <div className='bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100'>
+      <button onClick={toggleTheme}>
+        Switch to mode
+      </button>
       <ToastContainer/>
         <Navbar/>
         <Hero />
