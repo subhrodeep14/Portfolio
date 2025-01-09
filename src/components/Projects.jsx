d15299d98ba8dba127ba1501b6f663894b927878
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-01-09 21:59:11 +0530

    project animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,14 @@ function Projects() {
 
   return (
     <div id="projects" className="py-16 min-h-screen bg-zinc-900 text-gray-300">
-      <div className="container mx-auto ">
+      <motion.div
+      
+      initial={{opacity:0,y: -100}}
+      whileInView={{opacity:1,y: 0}}
+  
+      transition={{duration: 1.5}}
+      viewport={{once: true}}
+      className="container mx-auto ">
         <h2 className="text-5xl font-bold pt-5 text-center">My <span className="text-blue-500 underline underline-offset-4">Projects</span> </h2>
         <div className="flex justify-center mt-14 flex-wrap gap-8 pt-5">
             <Cards image={assets.real} description={'A real estate website that helps you find the best property for you'} techs={project1} text={'Real Estate'} pageLink={"https://real-estate-nine-blush.vercel.app/"} gitLink={'https://github.com/subhrodeep14/Real-Estate'}/>
@@ -18,7 +25,7 @@ function Projects() {
             
         </div>
 
-      </div>
+      </motion.div>
 
 
     </div>
