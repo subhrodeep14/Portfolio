c10bc7d9520ee68c9973692652c291a2b77aa31f
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-01-10 23:41:56 +0530

    skill updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import CardForSkills from "./CardForSkills";
 import {icon} from "../icons/icon"
+import {motion} from "framer-motion"
 function Skills() {
     const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Tailwind CSS','Express.js','MongoDb','Postgres','Prisma','Next.js'];
   
@@ -10,8 +11,15 @@ function Skills() {
           <h2 className="text-5xl font-bold  ">My <span className="text-blue-500 underline underline-offset-4">Skills</span> </h2>
           <p className="font-semibold text-zinc-500 mb-10 mt-5">Crafting full stack web apps using M.E.R.N stack</p>
           </div>
-          <div className="mt-3 flex flex-wrap  justify-center gap-10 ">
-            <div className="flex flex-wrap justify-center  gap-7 h-56 ">
+          <div
+          
+          className="mt-3 flex flex-wrap  justify-center gap-10 ">
+            <motion.div
+             initial={{opacity:0,x: -100}}
+             whileInView={{opacity:1,x: 0}}
+         
+             transition={{duration: 1.5}}
+             viewport={{once: true}} className="flex flex-wrap justify-center  gap-7 h-56 ">
             <CardForSkills text="Html" image={icon.html}/>
             <CardForSkills text="Css" image={icon.css}/>
             <CardForSkills text="Javascript" image={icon.javascript}/>
@@ -19,8 +27,13 @@ function Skills() {
             <CardForSkills text="Express" image={icon.express}/>
             <CardForSkills text="Mongodb" image={icon.mongodb}/>
 
-            </div>
-            <div className="flex flex-wrap justify-center gap-7 h-56 ">
+            </motion.div>
+            <motion.div 
+             initial={{opacity:0,x: 100}}
+             whileInView={{opacity:1,x: 0}}
+         
+             transition={{duration: 1.5}}
+             viewport={{once: true}} className="flex flex-wrap justify-center gap-7 h-56 ">
             
             <CardForSkills text="React" image={icon.react}/>
             <CardForSkills text="Tailwind" image={icon.tailwind}/>
@@ -28,7 +41,7 @@ function Skills() {
             <CardForSkills text="Prisma" image={icon.prisma}/>
             <CardForSkills text="Nextjs" image={icon.nextjs}/>
             <CardForSkills text="Postman" image={icon.postman}/>
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
