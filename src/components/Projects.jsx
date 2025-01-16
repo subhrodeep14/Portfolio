44cadc396f33c36c8ec1f1eb4a95023abd444ed8
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-01-16 23:22:45 +0530

    few update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,6 @@ function Projects() {
       
       initial={{opacity:0,y: -100}}
       whileInView={{opacity:1,y: 0}}
-  
       transition={{duration: 1.5}}
       viewport={{once: true}}
       className="container mx-auto ">
